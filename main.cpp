f8aa7998fd5e9f046e6d640e3988a4c9e5dfc62a
Max <github.ua.cv@gmail.com>
2025-11-15 15:24:11 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,17 +13,13 @@ int main(int argc, char *argv[]) {
 
     QMainWindow mainWindow;
 
-
     QPlainTextEdit *textEdit = new QPlainTextEdit();
-    QTextBlock *textblock = new QTextBlock();
-    QStatusBar *statusBar = new QStatusBar();
 
     QFont font("JetBrain Mono", 14);
     font.setFixedPitch(true);
     textEdit->setFont(font);
 
     mainWindow.setCentralWidget(textEdit);
-    mainWindow.setStatusBar(statusBar);
 
     QFont fontmenu("JetBrain Mono", 11);
     font.setFixedPitch(true);
@@ -31,16 +27,50 @@ int main(int argc, char *argv[]) {
 
     menuBar->setFont(fontmenu);
 
+    QStatusBar *statusBar = new QStatusBar();
+    mainWindow.setStatusBar(statusBar);
+
     QMenu *fileMenu = menuBar->addMenu("&File");
     fileMenu->addAction("&New File");
-    fileMenu->addAction("&Open File");
-    fileMenu->addAction("&Open Folder");
+
+    QAction *openFileAction = fileMenu->addAction("&Open File");
+    QObject::connect(openFileAction, &QAction::triggered, [&](){
+        QString fileName = QFileDialog::getOpenFileName(&mainWindow,
+                            "Open File", "", "All Files (*.*)");
+        if (!fileName.isEmpty()) {
+            QFile file(fileName);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                textEdit->setPlainText(file.readAll());
+            }
+        }
+    });
+
+    QAction *openFolderAction = fileMenu->addAction("&Open Folder");
+    QObject::connect(openFolderAction, &QAction::triggered, [&](){
+        QString folderName = QFileDialog::getExistingDirectory(&mainWindow,"Open Folder", "");
+    });
 
     QMenu *editMenu = menuBar->addMenu("&Edit");
     editMenu->addAction("&Run");
+    QAction *saveFileAction = editMenu->addAction("&Save File");
+    QObject::connect(saveFileAction, &QAction::triggered, [&](){
+        QString fileName = QFileDialog::getSaveFileName(&mainWindow,
+                            "Save File", "", "All Files (*.*)");
+        if (!fileName.isEmpty()) {
+            QFile file(fileName);
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                file.write(textEdit->toPlainText().toUtf8());
+                file.close();
+                statusBar->showMessage("Saved file: " + fileName, 5000);
+            } else {
+                statusBar->showMessage("Failed to save file!", 5000);
+            }
+        }
+    });
+
     editMenu->addAction("&Exit", QApplication::quit);
 
-    mainWindow.setWindowTitle("Simple Text Editor");
+    mainWindow.setWindowTitle("Simple text editor");
     mainWindow.resize(1200, 800);
     mainWindow.show();
 
